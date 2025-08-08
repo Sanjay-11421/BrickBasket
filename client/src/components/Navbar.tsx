dc20a79512d95f7e506a943d147bf1c568398b97
S Sanjay <sanjayprogrammer22@gmail.com>
2025-08-08 21:18:45 +0530

    some things are added

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,14 +5,14 @@ import { FaShoppingCart } from "react-icons/fa";
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-black/15 backdrop-blur-lg py-2.5 flex items-center justify-between font-lato border-b-1 border-b-black/10 px-3 lg:px-32">
+      <nav className="bg-black/25 sticky top-0 backdrop-blur-lg py-2.5 flex items-center justify-between font-lato border-b-0.5 border-b-black/5 px-3 lg:px-32">
         <div className="mx-2 flex items-center">
-          <TbBrandDatabricks className="text-white" />
-          <h1 className="text-white ml-1 text-lg font-extrabold tracking-wide">
+          <TbBrandDatabricks className="text-glow drop-shadow-lg text-2xl drop-shadow-light glowing" />
+          <h1 className="text-white ml-1 text-lg sm:text-xl font-extrabold tracking-wide glowing">
             BrickBasket
           </h1>
         </div>
-        <div className="text-white/60 space-x-4 text-xs mx-3 flex items-center">
+        <div className="text-white/60 hidden  space-x-6 text-xs mx-3 md:flex items-center">
           <button className="hover:text-white transition cursor-pointer">
             Home
           </button>
@@ -23,14 +23,14 @@ const Navbar = () => {
             Orders
           </button>
         </div>
-        <div className="flex items-center space-x-6 mx-4 text-white/80">
+        <div className="flex items-center space-x-4 md:space-x-6 md:mx-4 text-white/80">
           <button className="hover:text-white transition cursor-pointer">
             <CgProfile />
           </button>
           <button className="hover:text-white  transition cursor-pointer">
             <FaShoppingCart className="relative" />
-            <p className="text-xs absolute translate-x-1.5 -translate-y-0.5 font-extrabold shadow-2xl ">
-              3
+            <p className="text-xs absolute translate-x-1.5 -translate-y-0.5  font-extrabold shadow-2xl ">
+              0
             </p>
           </button>
         </div>
--- client/src/pages/User/Home.tsx
+++ client/src/pages/User/Home.tsx
@@ -1,9 +1,41 @@
 import Navbar from "../../components/Navbar";
+import { FaSearch } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div className="bg-gradient-to-br from-dark via-primary to-light h-[100vh]">
+    <div className="bg-gradient-to-br from-dark via-primary to-light min-h-[100vh]">
       <Navbar />
+      <div className="font-lato text-white flex flex-col  justify-center items-center min-h-[90vh] mx-6 sm:mx-4">
+        <h1 className="font-extrabold text-center text-2xl  md:text-4xl lg:w-[70%]">
+          Book Quality Construction Materials Online Fast, Reliable, On-Time
+          Delivery
+        </h1>
+        <p className="md:text-sm text-xs max-w-2xl mt-4 text-center text-white/60">
+          Your one-stop destination for high-quality, innovative, and
+          sustainable construction materials, delivered with futuristic
+          efficiency.
+        </p>
+        <div className="w-full flex justify-center items-center">
+          <div className="relative md:w-1/2 mt-8">
+            <input
+              className="w-full px-5 pr-14 py-2 rounded-2xl text-sm
+                 bg-white/5 backdrop-blur-lg border border-white/20
+                 text-white placeholder-white/70
+                 focus:outline-none focus:bg-white/10
+                 shadow-lg"
+              placeholder="Search for materials"
+            />
+
+            <button
+              className="absolute right-0 top-1/2 -translate-y-1/2
+                 bg-primary backdrop-blur-lg border border-white/20
+                 text-white px-5 py-2.5 rounded-2xl text-sm font-bold cursor-pointer"
+            >
+              <FaSearch className="text-white/70" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
